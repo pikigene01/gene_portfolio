681ec43029a13ce183cfb2efbc15f12e3858275f
pikigene01 <pikigene01@gmail.com>
2025-06-25 05:13:33 +0200

    add

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -62,13 +62,13 @@ export default function Portfolio() {
   const legacyApps = [
     {
       name: "Clear App",
-      image: "/assets/images/portfolio/img7.webp",
+      image: "/gene_portfolio/assets/images/portfolio/img7.webp",
       playStoreUrl: "https://play.google.com/store/apps/details?id=com.clearbuildingsolutions.care_app",
       category: "application development"
     },
     {
       name: "Piki Delivery",
-      image: "/assets/images/portfolio/img-3.jpg",
+      image: "/gene_portfolio/assets/images/portfolio/img-3.jpg",
       playStoreUrl: "https://apkpure.com/p/com.pikidelivery.pikideliveryapp",
       category: "application development"
     }
